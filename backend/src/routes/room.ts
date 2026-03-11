a0bbbadd114879aaa688cf09d23a1977709a2025
MarianoNLR <marianonehuenlr@gmail.com>
2026-03-10 23:30:22 -0300

    routes for rooms endpoints

---
+++ backend/src/routes/room.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import data, { Room } from "../data/store";
+
+const roomRouter = Router();
+
+roomRouter.get("/", (req, res) => {
+    console.log("GET /rooms");
+    const rooms : Room[] = Object.values(data.rooms)
+    return res.json({ rooms });
+});
+
+roomRouter.get("/:roomId", (req, res) => {
+    const { roomId } = req.params;
+    console.log(`GET /rooms/${roomId}`);
+    const room : Room = data.rooms[roomId];
+    return res.json({ room });
+});
+
+export default roomRouter;
