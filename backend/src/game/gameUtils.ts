48a990278b7758ee600a277157771b37ffe9e0d7
MarianoNLR <marianonehuenlr@gmail.com>
2026-03-25 23:33:01 -0300

    refactor: improve code formatting and structure in getGameState function

--- backend/src/game/gameUtils.ts
+++ backend/src/game/gameUtils.ts
@@ -1,19 +1,19 @@
-import { Room, Player } from '../data/store'
+import { Room } from '../types'
 import { roomRepository } from '../repositories/roomRepository'
 
 export function getGameState(roomId: string) {
-    const room : Room | null = roomRepository.findById(roomId);
+    const room: Room | null = roomRepository.findById(roomId);
     if (!room) {
         console.log(`Room ID ${roomId} does not exist.`);
         return null;
     }
-    const { game, ...rest } = room;
+    const { game, chat, ...rest } = room;
     if (!game) {
         console.log(`Game has not started for room ID ${roomId}.`);
         return null;
     }
-    const publicPlayers = room.players.map(({ id, nickname }) => ({ id, nickname }))
+    const publicPlayers = room.players.map(({ id, nickname }) => ({ id, nickname }));
 
-    const { impostorIds, word, votes, currentTimer,...publicGameState } = game;
-    return {...rest, players: publicPlayers, game: publicGameState};
+    const { impostorIds, word, votes, currentTimer, ...publicGameState } = game;
+    return { ...rest, players: publicPlayers, game: publicGameState };
 }
